e48ccae790fc38cc56a7330006d96902ea22fcb6
kom46 <kommunizm46@gmail.com>
2022-01-27 20:31:02 +0300

    fixed some defs, which must be in makefile

--- w25qxxConf.h
+++ w25qxxConf.h
@@ -4,7 +4,13 @@
 #define _W25QXX_SPI                   hspi1
 #define _W25QXX_CS_GPIO               FLASH_CS_GPIO_Port
 #define _W25QXX_CS_PIN                FLASH_CS_Pin
+// must be in makefile
+#ifndef _W25QXX_USE_FREERTOS
 #define _W25QXX_USE_FREERTOS          1
+#endif
+
+#ifndef _W25QXX_DEBUG
 #define _W25QXX_DEBUG                 0
+#endif
 
 #endif
